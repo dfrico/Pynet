0b9d1fe7b6de1a5af3a56bd1d45ce1b8da5818ee
Daniel Fernandez Rico <deferrico@gmail.com>
2015-12-28 18:04:07 +0100

    @pynet_ addition
    Also, ‘already common followers’ list is now a thing to avoid
    repetition.

--- public.py
+++ public.py
@@ -11,23 +11,20 @@ OAUTH_TOKEN_SECRET = ''
 
 twitter = tw(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 """
-json_data = twitter.get_followers_list(user_id='@main_user')
+json_data = twitter.get_followers_list(user_id='@pynet_')
 for user in json_data["users"]:
     print user["name"]
 print "\n"
 """
-
-json_ids = twitter.get_followers_ids(user_id='@main_user') # 1 request
+already = {''}
+json_ids = twitter.get_followers_ids(user_id='@pynet_')
 for user in json_ids["ids"]:
-    print user
-    print type(json_ids["ids"])
-    f_user1 = twitter.get_followers_ids(user_id=user) # followers of user 1. RATE LIMIT EXCEEDED. limit = 15
+    print twitter.show_user(user_id=user)["screen_name"]
+    f_user1 = twitter.get_followers_ids(user_id=user)  # followers of user 1. Limit = 15 users, add delay (1query/min)
     common_set = set(f_user1["ids"]) & set(json_ids["ids"])
     for user in common_set:
-        print "\t"
-        print user
+        if user not in already:
+            print "\t" + twitter.show_user(user_id=user)["screen_name"]
+            already.add(user)
 
-
-
-
-# edge if (user 2 is in) followers_main & followers_user1
+# to-do: avoid bidirectional edges (mutual follow)? Design decision. Check NX.
