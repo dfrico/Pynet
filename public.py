72861b5f4350ec8255e06f3b339e45910d927b68
Daniel Fernandez Rico <deferrico@gmail.com>
2015-12-30 19:07:45 +0100

    After cleaning.

--- public.py
+++ public.py
@@ -1,10 +1,6 @@
 import networkx as nx
 from twython import Twython as tw
 from datetime import datetime
-import matplotlib.pyplot as plt
-
-import os
-import cPickle as pickle
 
 print datetime.now()
 
@@ -28,11 +24,7 @@ for user in followers1["ids"]:
     G.add_edge('@pynet_', username)
     # time.sleep(60)
     already.add(user)
-    """
-    mapping = {i: user}
-    i += 1
-    G = nx.relabel_nodes(G, mapping)
-    """
+
     print username
 
     followers2 = twitter.get_followers_ids(user_id=user)
@@ -43,36 +35,4 @@ for user in followers1["ids"]:
             G.add_edge(twitter.show_user(user_id=follower)["screen_name"], username)
             print "\t" + twitter.show_user(user_id=follower)["screen_name"]
 
-
-# nx.draw_circular(G, with_labels=True, node_size=20, node_color='black')
-# plt.savefig("path.svg")
 nx.write_gexf(G, "graph.gexf")
-"""
-if not os.path.exists('newgraph.pickle'):
-    os.system('touch newgraph.pickle')
-with open('newgraph.pickle', 'w') as f:
-    pickle.dump(G, f)
-    print 'Pickled the graph'
-"""
-
-
-# edge if (user 2 is in) followers_main & followers_user1
-
-"""
-# twitter = tw(APP_KEY, APP_SECRET)
-# enter authentication tokens by hand, not by this
-# auth = twitter.get_authentication_tokens(callback_url='http://mysite.com/callback')
-# twitter.update_status(status='Testing twython & netpy...')
-
-# followers = twitter.get_followers_list(user_id='blayhem')
-# json_data = json.loads(twitter.get_followers_list(user_id='blayhem'))
-with open('../../follow.json') as f:
-    json_data = json.load(f)
-    # users = [x["name"] for x in json_data["name"]]
-    # for user in users: print user.name
-    # for user in json_data: print user
-    for user in json_data["users"]:
-        print user["name"]
-
-
-"""
