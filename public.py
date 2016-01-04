c6075b33deeef74fe0b026fcd2875f8a0e2cc9cb
Daniel Fernandez Rico <deferrico@gmail.com>
2016-01-04 10:10:53 +0100

    Pynet v1.2
    - Protected accounts are no longer a problem, the script simply skips
    them.
    - The user now introduces the tokens manually, and there’s a try-catch
    for handling wrong input.

--- public.py
+++ public.py
@@ -1,41 +1,55 @@
 import networkx as nx
 from twython import Twython as tw
+import twython
 from datetime import datetime
 from time import sleep
 
-print datetime.now()
+from networkx.readwrite import json_graph
+import json
 
+print datetime.now()
 G = nx.Graph()
 G.clear()
 
-APP_KEY = ''
-APP_SECRET = ''
+APP_KEY = raw_input("Enter app key: ")
+APP_SECRET = raw_input("Enter app secret key: ")
 # PRIVATE
-OAUTH_TOKEN = ''
-OAUTH_TOKEN_SECRET = ''
+OAUTH_TOKEN = raw_input("Enter personal key: ")
+OAUTH_TOKEN_SECRET = raw_input("Enter personal secret key: ")
 
 twitter = tw(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+main_user = raw_input("Enter your twitter @ username: ")
 
-already = {''}
-sleep(60)
-followers1 = twitter.get_followers_ids(user_id='blayhem')
-for user in followers1["ids"]:
-    username = twitter.show_user(user_id=user)["screen_name"]
-    G.add_node(username)
-    G.add_edge('@blayhem', username)
-    # for i in tqdm(range(60)):
+"""
+main_user has FOLLOWERS1, where each USER and its USERNAME
+has FOLLOWERS2, where each USER2 has a USERNAME2.
+"""
+try:
+    already = {''}
+    followers1 = twitter.get_followers_ids(user_id=main_user)
     sleep(60)
 
-    already.add(user)
+    for user in followers1["ids"]:
+        username = twitter.show_user(user_id=user)["screen_name"]
+        G.add_node(username)
+        G.add_edge(main_user, username)
+        # for i in tqdm(range(60)):
+        sleep(60)
 
-    print username
+        already.add(user)
+        print username
 
-    followers2 = twitter.get_followers_ids(user_id=user)
-    G.node[username]['weight'] = len(followers2)
+        if not twitter.show_user(user_id=user)["protected"]:
+            followers2 = twitter.get_followers_ids(user_id=user)
+            # G.node[username]['weight'] = len(followers2)
+            for user2 in (set(followers2["ids"]) & set(followers1["ids"])):
+                if user2 not in already:
+                    username2 = twitter.show_user(user_id=user2)["screen_name"]
+                    G.add_edge(username2, username)
+                    print "\t" + username2
 
-    for follower in (set(followers2["ids"]) & set(followers1["ids"])):
-        if follower not in already:
-            G.add_edge(twitter.show_user(user_id=follower)["screen_name"], username)
-            print "\t" + twitter.show_user(user_id=follower)["screen_name"]
+        nx.write_gexf(G, "graph.gexf")  # networkx export to gexf for Gephi.
+        json.dumps(json_graph.node_link_data(G))  # json export to sigma JS
 
-nx.write_gexf(G, "graph.gexf")
+except twython.exceptions.TwythonAuthError:
+    print "Error in the tokens."
