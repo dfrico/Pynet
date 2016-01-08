daf6551c6b6e332200a0b95d367d419319ba5eeb
Daniel Fernandez Rico <deferrico@gmail.com>
2016-01-08 15:57:41 +0100

    Exceptions & weight added.
    Also some prints to clarify when the scripts runs correctly.

--- public.py
+++ public.py
@@ -1,9 +1,9 @@
 import networkx as nx
-from twython import Twython as tw
+from twython import Twython as Tw
 import twython
 from datetime import datetime
 from time import sleep
-
+import os
 from networkx.readwrite import json_graph
 import json
 
@@ -17,7 +17,7 @@ APP_SECRET = raw_input("Enter app secret key: ")
 OAUTH_TOKEN = raw_input("Enter personal key: ")
 OAUTH_TOKEN_SECRET = raw_input("Enter personal secret key: ")
 
-twitter = tw(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+twitter = Tw(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 main_user = raw_input("Enter your twitter @ username: ")
 
 """
@@ -27,13 +27,17 @@ has FOLLOWERS2, where each USER2 has a USERNAME2.
 try:
     already = {''}
     followers1 = twitter.get_followers_ids(user_id=main_user)
+    print("\nUp and running! Please grab a coffee, this is going to take 1min per follower.\n")
     sleep(60)
+
+    if not os.path.exists('graph.json'):
+            os.system('touch graph.json')
 
     for user in followers1["ids"]:
         username = twitter.show_user(user_id=user)["screen_name"]
-        G.add_node(username)
+        # G.add_node(username)
         G.add_edge(main_user, username)
-        # for i in tqdm(range(60)):
+        # for i in tqdm(range(60)): TO-DO: INSERT PROGRESS BAR INSTEAD OF PRINTING THE USER NAMES.
         sleep(60)
 
         already.add(user)
@@ -41,7 +45,9 @@ try:
 
         if not twitter.show_user(user_id=user)["protected"]:
             followers2 = twitter.get_followers_ids(user_id=user)
-            # G.node[username]['weight'] = len(followers2)
+
+            G.node[username]['Size'] = len(followers2["ids"])
+
             for user2 in (set(followers2["ids"]) & set(followers1["ids"])):
                 if user2 not in already:
                     username2 = twitter.show_user(user_id=user2)["screen_name"]
@@ -49,7 +55,14 @@ try:
                     print "\t" + username2
 
         nx.write_gexf(G, "graph.gexf")  # networkx export to gexf for Gephi.
-        json.dumps(json_graph.node_link_data(G))  # json export to sigma JS
 
+        with open('graph.json', 'w') as f:
+            f.write(json.dumps(json_graph.node_link_data(G)))   # json export to sigma JS
+
+    print("\n END OF FILE \n")
 except twython.exceptions.TwythonAuthError:
-    print "Error in the tokens."
+    print "\nError in the tokens."
+except twython.exceptions.TwythonRateLimitError:
+    print "\nRate limit exceeded: too much requests. Please wait 15 min and try again."
+except twython.exceptions.TWITTER_HTTP_STATUS_CODE:
+    print "\nError connecting. Check your internet connection."
