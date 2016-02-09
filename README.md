8c84b36132c5248ab9342a5c0ed822e572a7a613
Daniel Fernandez Rico <deferrico@gmail.com>
2016-02-09 15:17:56 +0100

    Pynet 2.0

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
 # Pynet
 ## A python module for Twitter data visualization.
 
+##### Update:
+You **need** to create an ```init.py``` file with the following structure:
+```
+from pynet import Pynet as py
+APP_KEY =             [list of (your) token(s) for the APP_KEY]
+APP_SECRET =          [list of (your) token(s) for the APP_SECRET]
+# PRIVATE
+OAUTH_TOKEN =         [list of (your) token(s) for the OAUTH_TOKEN]
+OAUTH_TOKEN_SECRET =  [list of (your) token(s) for the OAUTH_TOKEN_SECRET]
+pynet = py()
+pynet.pynet(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+```
+
+==================================================
+
 Pynet is a project still in progress. It allows you to visualize your twitter connections, people who follow you and follow each other, as a graph.
-To run it, type
+1. Type
 ```
 git clone https://github.com/blayhem/pynet pynet
 ```
-First, you have to create a [Twitter App](http://apps.twitter.com/) for your user.
+2. You have to create a [Twitter App](http://apps.twitter.com/) for your user.
 Give it a name, a description, and go to the tokens tab to obtain your user tokens.
-With the four tokens at hand, run the script as follows:
+
+3. With the four tokens at hand, follow the steps in the update (creating the ```init.py``` file) and run it as
 ```
-python pynet.py
+python init.py
 ```
-Copy the tokens as asked, and then follow the instructions.
+
 
 At the end, you'll get a .gexf file, which you have to open with [Gephi](https://gephi.org).
 There, rearrange the nodes with some graph-drawing algorithms, such as the Fruchterman-Reingold algorithm, and color the graph as preferred.
 
-##### Here's the [link](http://blayhem.github.io/post/Visualizing%20Data%20with%20Pynet/) to my blog, where I explain the process of creating Pynet.
+##### Here's the [link](http://blayhem.github.io/blog/post/Visualizing%20Data%20with%20Pynet/) to my blog, where I explain the process of creating Pynet.
 
-###### Disclaimer: it's still very in beta. Although it works.
-###### Disclaimer 2: if you have a lot of followers, be patient. It takes a lot of time.
+###### Disclaimer: it's still in beta. Although it works.
--- pynet.py
+++ pynet.py
@@ -1,72 +1,115 @@
+# coding=utf-8
 import networkx as nx
-from twython import Twython as Tw
 import twython
-from datetime import datetime
 from time import sleep
 import os
+# JSON imports
 from networkx.readwrite import json_graph
-import json
+import json                         # TO-DO: review imports
 
-print datetime.now()
-G = nx.Graph()
-G.clear()
 
-APP_KEY = raw_input("Enter app key: ")
-APP_SECRET = raw_input("Enter app secret key: ")
-# PRIVATE
-OAUTH_TOKEN = raw_input("Enter personal key: ")
-OAUTH_TOKEN_SECRET = raw_input("Enter personal secret key: ")
+class Pynet(object):
+    def pynet(self, APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET):
+        try:
+            G = nx.Graph()
+            main_user = raw_input("Enter your twitter @ username: ")
+            # main_user = 'pynet_'
+            rate = 0
+            i = 1
 
-twitter = Tw(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-main_user = raw_input("Enter your twitter @ username: ")
+            twitter = twython.Twython(APP_KEY[i], APP_SECRET[i], OAUTH_TOKEN[i], OAUTH_TOKEN_SECRET[i])
+            already = {''}
+            musn = twitter.show_user(screen_name=main_user)["id"]  # Main User Screen Name to get ID
+            followers1 = twitter.get_followers_ids(user_id=musn)
+            time = twitter.show_user(screen_name=main_user)["followers_count"]  # Estimated time in min (= number of followers)
+
+            if time/len(APP_KEY) == 1:
+                print("\nUp and running! Please grab a coffee, this is going to take "+str(time/len(APP_KEY)) +
+                      " minute ¯\_(ツ)_/¯\n")
+            else:
+                print("\nUp and running! Please grab a coffee, this is going to take "+str(time/len(APP_KEY)) +
+                      " minutes ¯\_(ツ)_/¯\n")
+
+            sleep(60/(len(APP_KEY)))
+
+            if not os.path.exists('graph.json'):
+                os.system('touch graph.json')
+
+            for user in followers1["ids"]:
+
+                """
+                main_user has FOLLOWERS1, where each USER and its USERNAME
+                has FOLLOWERS2, where each USER2 has a USERNAME2.
+                """
+
+                username = twitter.show_user(user_id=user)["screen_name"]
+                G.add_edge(main_user, username)
+                # for i in tqdm(range(60)): TO-DO: INSERT PROGRESS BAR INSTEAD OF PRINTING THE USER NAMES.
+                sleep(60/(len(APP_KEY)))
+
+                already.add(user)
+                print username
+
+                if not twitter.show_user(user_id=user)["protected"]:
+                    followers2 = twitter.get_followers_ids(user_id=user)
+
+                    G.node[username]['Size'] = len(followers2["ids"])
+
+                    for user2 in (set(followers2["ids"]) & set(followers1["ids"])):
+                        if user2 not in already:
+                            username2 = twitter.show_user(user_id=user2)["screen_name"]
+                            G.add_edge(username2, username)
+                            print "\t" + username2
+
+                nx.write_gexf(G, "graph.gexf")  # networkx export to gexf for Gephi.
+
+                with open('graph.json', 'w') as f:
+                    f.write(json.dumps(json_graph.node_link_data(G)))   # json export to sigma JS
+
+                rate += 1
+
+                if rate == 14:
+                    if i < len(APP_KEY):
+                        i += 1
+                    else:
+                        i = 0
+                    twitter = twython.Twython(APP_KEY[i], APP_SECRET[i], OAUTH_TOKEN[i], OAUTH_TOKEN_SECRET[i])
+                    rate = 0
+            print("\n END OF FILE. Check .gexf file.\n")
+
+        except twython.exceptions.TwythonAuthError:
+            print "\nError in the tokens."
+        except twython.exceptions.TwythonRateLimitError:
+            print "\nRate limit exceeded: too much requests. Please wait 15 min and try again."
+        except twython.exceptions.TWITTER_HTTP_STATUS_CODE:
+            print "\nError connecting. Check your internet connection."
+        except twython.exceptions.TwythonError:
+            print("\nGeneric error ¯\_(ಥ_ಥ)_/¯")
+        except KeyboardInterrupt:
+            print "\nService manually stopped."
+
+
 
 """
-main_user has FOLLOWERS1, where each USER and its USERNAME
-has FOLLOWERS2, where each USER2 has a USERNAME2.
+if not os.path.exists('newgraph.pickle'):
+    os.system('touch newgraph.pickle')
+with open('newgraph.pickle', 'w') as f:
+    pickle.dump(G, f)
+    print 'Pickled the graph'
+
+# twitter = Tw(APP_KEY, APP_SECRET)
+# enter authentication tokens by hand, not by this
+# auth = twitter.get_authentication_tokens(callback_url='http://mysite.com/callback')
+# twitter.update_status(status='Testing twython & netpy...')
+
+# followers = twitter.get_followers_list(user_id='blayhem')
+# json_data = json.loads(twitter.get_followers_list(user_id='blayhem'))
+with open('../../follow.json') as f:
+    json_data = json.load(f)
+    # users = [x["name"] for x in json_data["name"]]
+    # for user in users: print user.name
+    # for user in json_data: print user
+    for user in json_data["users"]:
+        print user["name"]
+
 """
-try:
-    already = {''}
-    musn = twitter.show_user(screen_name=main_user)["id"]  # Main User Screen Name to get ID
-    followers1 = twitter.get_followers_ids(user_id=musn)
-    time = twitter.show_user(screen_name=main_user)["followers_count"]  # Estimated time in min (= number of followers)
-
-    print("\nUp and running! Please grab a coffee, this is going to take "+str(time)+" minutes.\n")
-    sleep(60)
-
-    if not os.path.exists('graph.json'):
-            os.system('touch graph.json')
-
-    for user in followers1["ids"]:
-        username = twitter.show_user(user_id=user)["screen_name"]
-        G.add_edge(main_user, username)
-        # for i in tqdm(range(60)): TO-DO: INSERT PROGRESS BAR INSTEAD OF PRINTING THE USER NAMES.
-        sleep(60)
-
-        already.add(user)
-        print username
-
-        if not twitter.show_user(user_id=user)["protected"]:
-            followers2 = twitter.get_followers_ids(user_id=user)
-
-            G.node[username]['Size'] = len(followers2["ids"])
-
-            for user2 in (set(followers2["ids"]) & set(followers1["ids"])):
-                if user2 not in already:
-                    username2 = twitter.show_user(user_id=user2)["screen_name"]
-                    G.add_edge(username2, username)
-                    print "\t" + username2
-
-        nx.write_gexf(G, "graph.gexf")  # networkx export to gexf for Gephi.
-
-        with open('graph.json', 'w') as f:
-            f.write(json.dumps(json_graph.node_link_data(G)))   # json export to sigma JS
-
-    print("\n END OF FILE. Check .gexf file.\n")
-except twython.exceptions.TwythonAuthError:
-    print "\nError in the tokens."
-except twython.exceptions.TwythonRateLimitError:
-    print "\nRate limit exceeded: too much requests. Please wait 15 min and try again."
-except twython.exceptions.TWITTER_HTTP_STATUS_CODE:
-    print "\nError connecting. Check your internet connection."
-except twython.exceptions.TwythonError:
-    print("\nGeneric error.")
